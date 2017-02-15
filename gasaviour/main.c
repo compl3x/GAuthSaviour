c30f5440980354ec6d6d2be99804957a2b5a2cd2
Michael Cowell <michael.cowell@tutanota.com>
2017-02-15 00:29:12 +0000

    Efficiency/optimisations

--- gasaviour/main.c
+++ gasaviour/main.c
@@ -2,78 +2,61 @@
 #include <string.h>
 #include "sqlite3.h"
 #include <getopt.h>
-
+#include <stdlib.h>
 
 #ifndef VERSION
-    #define VERSION "1.2.7.0"
+    #define VERSION "1.3"
 #endif // VERSION
 
-/** String comparison
- *
- * \param first     The first string
- * \param second    The...second string? Why am I even commenting this?
- * \return int      1 if equal, otherwise 0
- */
-int isEqual(char first[], char second[]) {
-    return (strcmp(first,second) == 0);
-}
-
-// Function for exporting authentication data directly to a file
-/** Export auths to WinAuth format
- *
- * \param dbFile    The database file to read auths from
- * \param destFile  The WinAuth file to save to
- */
-void exportAuthsToWinAuth(char dbFile[], char destFile[]) {
-    // Open database
-    sqlite3 *db;
+void extractAuths(char dbFile[], char destFile[]) {
+	// Declare sql objects
+	sqlite3 *db;
     sqlite3_open(dbFile, &db);
     sqlite3_stmt *statement;
-    if (sqlite3_prepare_v2(db, "SELECT type, email, secret FROM accounts",-1,&statement,NULL) == SQLITE_OK) {
-        int res;
-        FILE *fp;
-        fp = fopen(destFile,"a");
-        // Loop through each authentication
-        while ( (res = sqlite3_step(statement)) != SQLITE_DONE) {
-            char buff[1024];
-            // Format and write to file
-            sprintf(buff,"otpauth://%s/%s?secret=%s\n",
-                    isEqual(sqlite3_column_blob(statement,0),"0") ? "totp" : "hotp",
-                    sqlite3_column_blob(statement,1),
-                    sqlite3_column_blob(statement,2));
-            fputs(buff,fp);
-        }
-    }
-    sqlite3_close(db);
-}
+	
+	int export = (destFile != NULL);
+	FILE *fp;
+	
+	if (export) {
+		fp = fopen(destFile,"a+");
+	}
 
-// Print all auths from the provided file
-/** Print all auths from dbFile
- *
- * \param dbFile The location of the database to read.
- */
-void printAllAuths(char dbFile[]) {
-    int res;
-    sqlite3 *db;
-    sqlite3_open(dbFile, &db);
-    sqlite3_stmt *statement;
-
-    if (sqlite3_prepare_v2(db,"SELECT email, secret,type FROM accounts",-1, &statement, NULL) == SQLITE_OK) {
-        while ( (res = sqlite3_step(statement)) != SQLITE_DONE) {
-            printf("\n\nEmail: %s",sqlite3_column_blob(statement,0));
-            printf("\nSecret: %s",sqlite3_column_blob(statement,1));
-            printf("\nType: %s",isEqual(sqlite3_column_blob(statement,2),"0") ? "totp" : "hotp");
-        }
-    }
-    sqlite3_close(db);
+	// Loop through 'accounts'
+	if (sqlite3_prepare_v2(db, "SELECT type, email, secret FROM accounts",-1,&statement,NULL) == SQLITE_OK) {
+		int res;
+		while ( (res = sqlite3_step(statement)) != SQLITE_DONE) {
+			if (export) {
+				char buffer[256];
+				sprintf(buffer,"otpauth://%s/%s?secret=%s\n",
+						atoi(sqlite3_column_blob(statement,0)) == 0 ? "totp" : "hotp",
+						sqlite3_column_blob(statement,1),
+						sqlite3_column_blob(statement,2));
+				fputs(buffer, fp);
+			}
+			else {
+				printf("\n\nType: %s",atoi(sqlite3_column_blob(statement,0)) == 0 ? "totp" : "hotp");
+				printf("\nEmail: %s",sqlite3_column_blob(statement,1));
+				printf("\nSecret: %s",sqlite3_column_blob(statement,2));
+			}
+		}
+	}
+	
+	// Gracefully close file and free pointer
+	if (export) {
+		fclose(fp);
+		free(fp);
+		printf("\nExported to: %s", destFile);
+	}
+	
+	sqlite3_close(db);
 }
 
 void main(int argc, char *argv[]) {
     printf("GAuthSaviour %s by Michael Cowell (github/compl3x) (@Complex360)",VERSION);
     char * fileName = NULL;
     char * destFileName = NULL;
-
     int arg;
+	
     while ((arg = getopt(argc, argv, "df:w:d")) != -1) {
         switch(arg) {
         case 'f':
@@ -83,7 +66,7 @@ void main(int argc, char *argv[]) {
             destFileName = optarg;
             break;
         case 'd':
-            // This currently doesn't check if the file exists, and yeah yeah I know system sucks but so does life.
+            // TODO: This currently doesn't check if the file exists
             printf("\n\n");
             system("adb pull /data/data/com.google.android.apps.authenticator2/databases/databases");
             fileName = "databases";
@@ -91,19 +74,14 @@ void main(int argc, char *argv[]) {
         }
     }
 
-    if (destFileName != NULL) {
-        printf("\n[+] Converting %s to %s",fileName,destFileName);
-        exportAuthsToWinAuth(fileName,destFileName);
-    }
-    else if (fileName != NULL) {
-        printf("\n[+] Printing auths from %s",fileName);
-        printAllAuths(fileName);
-    }
+    if (fileName != NULL) {
+		extractAuths(fileName, destFileName);
+	}
     else {
         printf("\n\nUsage:\t%s [-d] [-f <file>] [-w <file>]",argv[0]);
         printf("\n\n\t-d\t\tDump database via adb (must have adb in directory of PATH)");
         printf("\n\t-f <file>\tRead database from <file>");
-        printf("\n\t-w <file>\tConvert database to Winauth <file>\n");
+        printf("\n\t-w <file>\tConvert database to Winauth/plain text <file>\n");
     }
 
 }
